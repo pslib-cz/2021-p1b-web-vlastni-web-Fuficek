e7e1a4c01b10cc274f438cc3f519bbcfd9e4c8eb
Fuficek <matalacek49fuf@gmail.com>
2022-05-28 00:19:14 +0200

    mail.php funguje na 99%

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ if(isset( $_POST['subject']))
 $subject = $_POST['subject'];
 
 $content="From: $name \n Email: $email \n Message: $message";
-$recipient = "matalacek49fuf@gmail.com";
+$recipient = "astastny268@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo '
