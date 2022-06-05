e7c8188d2247924c8d59810e6b3922958f3fba1b
Fuficek <matalacek49fuf@gmail.com>
2022-06-05 12:58:13 +0200

    vypnutí animací nefunguje, shop added

---
+++ toggle.js
@@ -0,0 +1,15 @@
+const background = document.querySelector(".animate");
+const button = document.querySelector(".anim_toggle")
+
+button.addEventListener("click", () => {
+    background.classList.toggle("animate")
+});
+
+<script>
+const background = document.querySelector(".space");
+const button = document.querySelector(".anim_toggle")
+
+button.addEventListener("click", () => {
+background.classList.toggle("animate")
+});
+</script>
